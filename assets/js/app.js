8bd5321c7ddc05014cbe36f1047f3cd31ebb2651
Vinicius Cainelli <vicainelli@gmail.com>
2015-03-14 08:17:52 -0300

    manipulação da overlay

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+var overlay = document.getElementById('overlay');
+
+function openModal(){
+	overlay.classList.remove("is-hidden");
+}
+
+function closeModal(){
+	overlay.classList.add("is-hidden");
+}
